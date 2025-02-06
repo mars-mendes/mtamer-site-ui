f58242e02d01bf38ef0e42663dae50f977859782
Mars Mendes <mars@ibm.com>
2025-02-06 05:48:01 -0300

    Revert "test: preview img path"
    This reverts commit 5cec79077d1a9f183ec40b65b9f837dfcbbc2812.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "", //keep this only on dev
+    basePath: "/mtamer-site-ui", //keep this only on dev
     output: "export",
     assetPrefix: "./",
     reactStrictMode: false,
