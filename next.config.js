28007bc7170a61bf9f6cc5d45f792c999086035a
Mars Mendes <mars@ibm.com>
2025-02-07 01:01:12 -0300

    fix: next.config syntax and preview image test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'mtamer-site-ui';
+const repoName = '/mtamer-site-ui';
 
 const nextConfig = {
-  basePath: isProd ? /${repoName} : '',
-  assetPrefix: isProd ? /${repoName}/ : '',
+  basePath: isProd ? repoName : '',
+  assetPrefix: isProd ? repoName : './',
   output: 'export',
   reactStrictMode: true,
   images: {
