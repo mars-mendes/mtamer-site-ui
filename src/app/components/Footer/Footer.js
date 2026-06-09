b6f731f3cb224a45930dcbda48ab7249e46dc698
Mars Mendes <mars@ibm.com>
2026-06-09 11:53:31 -0300

    fix: endereço no componente do mapa

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -28,7 +28,7 @@ export default function Footer(content) {
                 </div>
                 <div className="col-1 p-0" style={{maxWidth: "1rem"}}/>
                 <div className={`col-lg-4 col-md-6 col-12 ${styles.map}`}>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.6561083961965!2d-46.686012323838305!3d-23.580791378786614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce572182d42743%3A0xab840c3d4da18251!2sLaw%20Works%20%7C%20Coworking%20Jur%C3%ADdico!5e0!3m2!1spt-BR!2sbr!4v1781016413456!5m2!1spt-BR!2sbr"  width="100%" height="100%" style={{ border: 0, aspectRatio: "1/1", paddingRight: "1rem" }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"/>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d914.1641015020733!2d-46.684666324239714!3d-23.580780695255555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce57c55eb3c63d%3A0x426ac84b924bae42!2sMaur%C3%ADcio%20Tamer%20Advogados!5e0!3m2!1spt-BR!2sbr!4v1781016732151!5m2!1spt-BR!2sbr" width="100%" height="100%" style={{ border: 0, aspectRatio: "1/1", paddingRight: "1rem" }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"/>
                 </div>
             </div>
         </>
